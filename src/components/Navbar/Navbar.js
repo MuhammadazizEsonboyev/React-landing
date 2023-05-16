686f250adb552f059786085744bf3bab297aac2c
MuhammadazizEsonboyev <esonboyev03@bk.ru>
2023-05-16 15:37:13 +0500

    fix

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,102 +1,106 @@
-import React, {useState, useEffect} from 'react'
-import { Nav, 
-    NavbarContainer, 
-    NavLogo, 
-    NavIcon, 
-    HamburgerIcon,
-    NavMenu,
-    NavItem,
-    NavLinks,
-    NavItemBtn,
-    NavBtnLink
- } from './Navbar.elements'
-import { FaTimes, FaBars } from 'react-icons/fa';
+import React, { useState, useEffect } from 'react'
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  NavIcon,
+  HamburgerIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavItemBtn,
+  NavBtnLink
+} from './Navbar.elements'
+import { FaTimes, FaBars } from 'react-icons/fa'
 import { IconContext } from 'react-icons/lib'
-import { Button } from '../../globalStyles';
+import { Button } from '../../globalStyles'
 
+function Navbar () {
+  const [click, setClick] = useState(false)
+  const [button, setButton] = useState(true)
 
-function Navbar() {
+  const [homeClick, setHomeClick] = useState(false)
+  const [servicesClick, setServicesClick] = useState(false)
+  const [productsClick, setProductsClick] = useState(false)
 
-    const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
+  console.log(servicesClick)
+  const handleHomeClick = () => {
+    setHomeClick(true)
+    setProductsClick(false)
+    setServicesClick(false)
+  }
+  const handleProductsClick = () => {
+    setHomeClick(false)
+    setProductsClick(true)
+    setServicesClick(false)
+  }
 
-    const [homeClick, setHomeClick] = useState(false);
-    const [servicesClick, setServicesClick] = useState(false);
-    const [productsClick, setProductsClick] = useState(false);
+  const handleClick = () => setClick(!click)
 
-    const handleHomeClick = () => {
-        setHomeClick(true);
-        setProductsClick(false);
-        setServicesClick(false);
+  const closeMobileMenu = () => setClick(false)
+
+  const showButton = () => {
+    // so if the screensize is <= 960px then set button state to false
+    if (window.innerwidth <= 960) {
+      setButton(false)
+    } else {
+      setButton(true)
     }
-    const handleProductsClick = () => {
-        setHomeClick(false);
-        setProductsClick(true);
-        setServicesClick(false);
-    }
+  }
 
-    const handleClick = () =>  setClick(!click);
-    
-    const closeMobileMenu = () => setClick(false);
+  useEffect(() => {
+    showButton()
+  }, [])
 
-    const showButton = () => {
-        // so if the screensize is <= 960px then set button state to false
-        if(window.innerwidth <= 960) {
-            setButton(false)
-        } else {
-            setButton(true)
-        }
-    }
+  window.addEventListener('resize', showButton)
 
-    useEffect(() => {
-        showButton();
-    }, [])
+  return (
+    <>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <Nav>
+          <NavbarContainer>
+            <NavLogo to='/'>
+              <NavIcon />
+              ULTRA
+            </NavLogo>
+            <HamburgerIcon onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </HamburgerIcon>
+            <NavMenu onClick={handleClick} click={click}>
+              <NavItem onClick={handleHomeClick} homeClick={homeClick}>
+                <NavLinks to='/' onClick={closeMobileMenu}>
+                  Home
+                </NavLinks>
+              </NavItem>
 
-    window.addEventListener('resize', showButton);
+              <NavItem
+                onClick={handleProductsClick}
+                productsClick={productsClick}
+              >
+                <NavLinks to='/Products' onClick={closeMobileMenu}>
+                  Products
+                </NavLinks>
+              </NavItem>
 
-    return (
-        <>
-        <IconContext.Provider value={{ color: '#fff' }}>
-            <Nav>
-                <NavbarContainer>
-                    <NavLogo to='/'> 
-                        <NavIcon />
-                            ULTRA
-                    </NavLogo>
-                    <HamburgerIcon onClick={handleClick}>
-                        {click ? <FaTimes /> : <FaBars />}
-                    </HamburgerIcon>
-                    <NavMenu onClick={handleClick} click={click} >
-                        <NavItem onClick={handleHomeClick} homeClick={homeClick}>
-                            <NavLinks to='/' onClick={closeMobileMenu}>
-                                Home
-                            </NavLinks>
-                        </NavItem>
-                    
-                        <NavItem onClick={handleProductsClick} productsClick={productsClick}>
-                            <NavLinks to='/Products' onClick={closeMobileMenu}>
-                                Products
-                            </NavLinks>
-                        </NavItem>
-
-                        <NavItemBtn >
-                            {button ? (
-                                <NavBtnLink to='/sign-up'>
-                                    <Button primary>SIGN UP</Button>
-                                </NavBtnLink>
-                            ) : (
-                                <NavBtnLink to='/sign-up'>
-                                    <Button onClick={closeMobileMenu} fontBig primary>SIGN UP</Button>
-                                </NavBtnLink>
-                            )}
-                            
-                        </NavItemBtn>
-                    </NavMenu>
-                </NavbarContainer>
-            </Nav>
-        </IconContext.Provider>    
-        </>
-    )
+              <NavItemBtn>
+                {button ? (
+                  <NavBtnLink to='/sign-up'>
+                    <Button primary>SIGN UP</Button>
+                  </NavBtnLink>
+                ) : (
+                  <NavBtnLink to='/sign-up'>
+                    <Button onClick={closeMobileMenu} fontBig primary>
+                      SIGN UP
+                    </Button>
+                  </NavBtnLink>
+                )}
+              </NavItemBtn>
+            </NavMenu>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
+    </>
+  )
 }
 
 export default Navbar
